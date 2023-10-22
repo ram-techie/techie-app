0ebc27df9951dbca3981b6ca1070473a457c2b3f
Ram Kumar Rayala <rrk.techie@gmail.com>
2023-10-22 14:43:28 +0530

    Create home page using routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "",
+    pathMatch : 'full',
+    redirectTo : 'home'
+  },
+  { 
+    path: "home",
+    loadChildren: () => import('./home-page/home-page.module').then( m => m.HomePageModule)
+  },
+  { 
+    path: "login",
+    loadChildren: () => import('./login-page/login-page.module').then( m => m.LoginPageModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
